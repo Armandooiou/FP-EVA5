f5a0af4bc824fef770d5968a3d5f378de7d1ea33
Armandooiou <L24550279@chihuahua2.tecnm.mx>
2024-08-31 17:43:49 -0600

    Add files via upload

---
+++ Eva1_4_TELEVICION/src/main/java/com/mycompany/eva1_4_televicion/Eva1_4_TELEVICION.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_televicion;
+
+/**
+ *
+ * @author aaro1
+ */
+public class Eva1_4_TELEVICION {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    //DECLARACION DE LAS VARIABLES
+    String marca;
+    String modelo;
+    int tama;
+    int year;
+    double precio;
+    
+    //ASIGNAR VALOR
+    marca ="samsumg";
+    year = 2024;
+    precio = 25000;
+    tama = 50;
+    modelo = "crystal";
+    
+    System.out.println(marca); //Imprimir el contenido de las variables 
+     System.out.println(year);
+     System.out.println(precio);
+     System.out.println(tama);
+      System.out.println(modelo); 
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
